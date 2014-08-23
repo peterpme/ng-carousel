4bf09a62c0e9f70f7e0696f5ef0fb3bc2dd51280
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-08-23 16:52:44 -0500

    minHeight calculator

--- angular-carousel.js
+++ angular-carousel.js
@@ -2,33 +2,48 @@
     angular.module('angular-carousel', [])
     .directive('angularCarousel', [function () {
 
-        function Carousel (element) {
+        function Carousel (element, arrowThreshold) {
             this.element = element;
+            this.arrowThreshold = arrowThreshold;
 
             this.currentIndex = 0;
 
-            this.arrows = this.element.find('.arrow', this.element);
             this.carouselBelt = this.element.find('.carousel-belt');
-
-            this.maxLength = this.carouselBelt.find('> div', this.carouselBelt).length;
-            this.slides = this.carouselBelt.find('.carousel-item', this.carouselBelt);
             this.carouselBelt.width((this.maxLength * 100) + '%');
+            this.maxLength = this.carouselBelt.find('> div', this.carouselBelt).length;
 
+            this.slides = this.carouselBelt.find('.carousel-item', this.carouselBelt);
+            this.arrows = this.element.find('.arrow', this.element);
+
+            this.calculateSlideHeight();
             this.requireArrows();
         }
 
         Carousel.prototype = {
+
+            calculateSlideHeight: function () {
+                var minHeight = 0;
+                this.slides.each(function (index) {
+                    if ( $(this).height() > minHeight ) {
+                        minHeight = $(this).height();
+                    }
+                    console.log(minHeight);
+                });
+            },
+
             nextSlide: function () {
                 if( this.currentIndex < this.maxLength ) {
                     ++this.currentIndex;
-                    this.animate();
+                    this.animateSlide();
+                    console.log(this.currentIndex);
                 }
             },
 
             previousSlide: function () {
                 if ( this.currentIndex > 0) {
                     --this.currentIndex;
-                    this.animate();
+                    this.animateSlide();
+                    console.log(this.currentIndex);
                 }
             },
 
@@ -40,7 +55,7 @@
             },
 
             requireArrows: function () {
-                if (this.maxLength <= this.arrowLimit) {
+                if (this.maxLength <= this.arrowThreshold) {
                     this.arrows.hide();
                 }
             }
@@ -54,9 +69,10 @@
 
                 var belt = angular.element('.carousel-belt', element),
                     leftArrow = angular.element('> .arrow-left', element),
-                    rightArrow = angular.element('> .arrow-right', element);
+                    rightArrow = angular.element('> .arrow-right', element),
+                    arrowThreshold = attrs.arrowThreshold;
 
-                    var carousel = new Carousel(element);
+                    var carousel = new Carousel(element, arrowThreshold);
 
                     rightArrow.on('click', element, function (e) {
                         e.preventDefault();
